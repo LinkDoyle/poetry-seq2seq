d9e31792017193f811bffcb98734aa07bc812e9d
Link <linkdoyle@163.com>
2020-04-17 11:26:57 +0800

    Optimize UI.

--- qtapp/app.py
+++ qtapp/app.py
@@ -1,10 +1,11 @@
 import sys
+from itertools import cycle
 from PySide2.QtWidgets import QApplication, QMainWindow, QMessageBox
 from PySide2.QtCore import QObject, QFile, QTranslator, Slot, QStringListModel
 from .ui_mainwindow import Ui_MainWindow
 from .predictor import Flags, Seq2SeqPredictor
+import time
 
-# from poetry import predict
 import os
 
 APP_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -50,13 +51,25 @@ class MainWindow(QMainWindow):
 
     @Slot(None, name='generate_poetry')
     def generate_poetry(self):
+        self.ui.button_generate.setEnabled(False)
+        app.processEvents()
+        begin_time = time.time_ns()
+
+        separators = cycle(('，', '。'))
+
         if self.keyword_model.rowCount() <= 0:
             QMessageBox.warning(self, self.APP_NAME, self.tr(
                 'Please add some keywords first!'))
             return
         keywords = self.keyword_model.stringList()
-        poetry = '\n'.join(self.predictor.predict(keywords))
+        lines = self.predictor.predict(keywords)
+        lines = [''.join(f'<b>{word}</b>' if word in keywords else word for word in line) for line in lines]
+        poetry = '\n'.join([f'<p>{line}{next(separators)}</p>' for line in lines])
         self.ui.label_poetry.setText(poetry)
+
+        elapsed_time = time.time_ns() - begin_time
+        self.ui.statusbar.showMessage(f'{self.tr("elapsed time: ")}{elapsed_time / 1000 / 1000:.2f} ms')
+        self.ui.button_generate.setEnabled(True)
 
 
 if __name__ == "__main__":
