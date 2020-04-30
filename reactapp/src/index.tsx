79919df9894ddd24ef0d9f938eca966395214680
Link <linkdoyle@163.com>
2020-04-30 17:41:24 +0800

    Update theme.

--- reactapp/src/index.tsx
+++ reactapp/src/index.tsx
@@ -3,15 +3,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "./theme";
 
 const TITLE = "AI Poetry Generator";
 ReactDOM.render(
   <React.StrictMode>
-    <Helmet>
-      <title>{TITLE}</title>
-    </Helmet>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Helmet>
+        <title>{TITLE}</title>
+      </Helmet>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
